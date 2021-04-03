c02c4229afc9e835d882fc3c41de8008bd862e47
Tina Chang <lucky_hope333@hotmail.com>
2021-04-02 20:48:52 -0700

    Integrated Third Party Scripts to build the component fiveStarRating.

--- force-app/main/default/lwc/fiveStarRating/fiveStarRating.js
+++ force-app/main/default/lwc/fiveStarRating/fiveStarRating.js
@@ -1,50 +1,78 @@
-//import fivestar static resource, call it fivestar
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import fivestar from '@salesforce/resourceUrl/fivestar';
+import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 
-// add constants here
+const ERROR_TITLE = 'Error loading five-star';
+const ERROR_VARIANT = 'error';
+const EDITABLE_CLASS = 'c-rating';
+const READ_ONLY_CLASS = 'readonly c-rating';
 
 export default class FiveStarRating extends LightningElement {
-  //initialize public readOnly and value properties
-  readOnly;
-  value;
+    //initialize public readOnly and value properties
+    @api readOnly = false;
+    @api value;
+    
+    editedValue;
+    isRendered;
+    ratingObj; // Is it needed?
 
-  editedValue;
-  isRendered;
-
-  //getter function that returns the correct class depending on if it is readonly
-  starClass() {}
-
-  // Render callback to load the script once the component renders.
-  renderedCallback() {
-    if (this.isRendered) {
-      return;
+    //getter function that returns the correct class depending on if it is readonly
+    get starClass() {
+        return (this.readOnly ? READ_ONLY_CLASS : EDITABLE_CLASS);
     }
-    this.loadScript();
-    this.isRendered = true;
-  }
 
-  //Method to load the 3rd party script and initialize the rating.
-  //call the initializeRating function after scripts are loaded
-  //display a toast with error message if there is an error loading script
-  loadScript() {}
+    // Render callback to load the script once the component renders.
+    renderedCallback() {
+        if (this.isRendered) {
+            return;
+        }
+        this.loadScript();
+        this.isRendered = true;
+    }
 
-  initializeRating() {
-    let domEl = this.template.querySelector('ul');
-    let maxRating = 5;
-    let self = this;
-    let callback = function (rating) {
-      self.editedValue = rating;
-      self.ratingChanged(rating);
-    };
-    this.ratingObj = window.rating(
-      domEl,
-      this.value,
-      maxRating,
-      callback,
-      this.readOnly
-    );
-  }
+    //Method to load the 3rd party script and initialize the rating.
+    //call the initializeRating function after scripts are loaded
+    //display a toast with error message if there is an error loading script
+    loadScript() {
+        Promise.all([
+            loadScript(this, fivestar + '/rating.js'),
+            loadStyle(this, fivestar + '/rating.css')
+        ])
+            .then(() => {
+                this.initializeRating();
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: ERROR_TITLE,
+                        message: error.message,
+                        variant: ERROR_VARIANT
+                    })
+                );
+            });    
+    }
 
-  // Method to fire event called ratingchange with the following parameter:
-  // {detail: { rating: CURRENT_RATING }}); when the user selects a rating
-  ratingChanged(rating) {}
+    initializeRating() {
+        let domEl = this.template.querySelector('ul');
+        let maxRating = 5;
+        let self = this;
+        let callback = function (rating) {
+            self.editedValue = rating;
+            self.ratingChanged(rating);
+        };
+        this.ratingObj = window.rating(
+            domEl,
+            this.value,
+            maxRating,
+            callback,
+            this.readOnly
+        );
+    }
+
+    // Method to fire event called ratingchange with the following parameter:
+    // {detail: { rating: CURRENT_RATING }}); when the user selects a rating
+    ratingChanged(rating) {
+        this.dispatchEvent(new CustomEvent('ratingchange', { rating: rating }));
+    }
 }
